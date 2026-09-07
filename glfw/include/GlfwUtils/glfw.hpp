eaa8d134ff494dd9f84df227dc38a628e374841b
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2026-09-07 19:11:28 +0200

    Feat(GlfwUtils): add initVulkanLoader

--- glfw/include/GlfwUtils/glfw.hpp
+++ glfw/include/GlfwUtils/glfw.hpp
@@ -14,4 +14,5 @@ auto createWindowSurface(const VkBindings::Instance &instance, GLFWwindow *windo
                          const VkBindings::AllocationCallbacks *allocator)
     -> std::expected<VkBindings::UniqueSurfaceKHR, VkBindings::Result>;
 
-};
+auto initVulkanLoader();
+}; // namespace GlfwUtils
--- glfw/src/glfw.cpp
+++ glfw/src/glfw.cpp
@@ -9,14 +9,17 @@
 
 #include <VkBindings/Concepts.hpp>
 #include <VkBindings/Enums.hpp>
+#include <VkBindings/Loader.hpp>
 #include <VkBindings/Objects.hpp>
 #include <VkBindings/ObjectsForward.hpp>
 #include <VkBindings/Structs.hpp>
+
 #include <VkUtils/Errorhandling.hpp>
 
 #include <expected>
 #include <utility>
 
+// NOLINTBEGIN(cppcoreguidelines-pro-type-reinterpret-cast)
 namespace GlfwUtils {
 auto createWindowSurface(const VkBindings::Instance &instance, GLFWwindow *window,
                          const VkBindings::AllocationCallbacks *allocator)
@@ -24,13 +27,16 @@ auto createWindowSurface(const VkBindings::Instance &instance, GLFWwindow *windo
     VkBindings::SurfaceKHR rawSurface;
 
     static_assert(VkBindings::Concepts::ABIIsHandle<VkBindings::SurfaceKHR>);
-    // NOLINTBEGIN(cppcoreguidelines-pro-type-reinterpret-cast)
     return VkUtils::succeeded(static_cast<VkBindings::Result>(glfwCreateWindowSurface(
                                   reinterpret_cast<VkInstance>(instance.getHandle()), window,
                                   reinterpret_cast<const VkAllocationCallbacks *>(allocator),
                                   reinterpret_cast<VkSurfaceKHR *>(&rawSurface))))
         .transform([&]() { return instance.adoptForignSurfaceKHR(std::move(rawSurface)); });
-    // NOLINTEND(cppcoreguidelines-pro-type-reinterpret-cast)
 }
 
+auto initVulkanLoader() {
+    glfwInitVulkanLoader(
+        reinterpret_cast<PFN_vkGetInstanceProcAddr>(VkBindings::Loader::GetGetInstanceProcAddr()));
+}
 }; // namespace GlfwUtils
+// NOLINTEND(cppcoreguidelines-pro-type-reinterpret-cast)
