099a22e2c81c53e39e407d9b6e80aa437b8e661e
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2026-09-15 10:24:46 +0200

    Fix(vma): just print leak info not all

--- vma/src/Vma.cpp
+++ vma/src/Vma.cpp
@@ -17,7 +17,7 @@
 #define VMA_DEBUG_INITIALIZE_ALLOCATIONS 1
 #define VMA_DEBUG_DETECT_CORRUPTION 1
 #define VMA_DEBUG_MARGIN 16
-#define VMA_DEBUG_LOG_FORMAT(format, ...)                                                          \
+#define VMA_LEAK_LOG_FORMAT(format, ...)                                                           \
     do {                                                                                           \
         printf((format), __VA_ARGS__);                                                             \
         printf("\n");                                                                              \
